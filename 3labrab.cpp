344f5666703d9f209829fa587e88f333bf454a5c
PrimeJedi <79159028+PrimeJedi@users.noreply.github.com>
2021-02-19 14:21:39 +0300

    Update 3labrab.cpp

--- 3labrab.cpp
+++ 3labrab.cpp
@@ -9,53 +9,35 @@
 */
 
 #include<iostream>
-#include<math.h>
-#include<ctime>
+#include<time.h>
+#include<stdlib.h>
+
 using namespace std;
 
-void get_matrix(  )
-{
-	int n = 3, m = 3; 
-	int matrix[n][m];
-	
-	for (int i = 1; i < n + 1; i++)
-	{
-		for (int j = 1; j < m + 1; j++)
-		{
-			matrix[i][j] = rand() % 10;
-		}
-	}
-	srand(time(0));
-}
-void print_matrix()
-{
-	int n = 3, m = 3;
-	int matrix[n][m];
-	
-	for (int i = 1; i < n + 1; i++)
-	{
-		for (int j = 1; j < m+ 1; j++)
-		{
-			cout << matrix[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	srand(time(0));
-}
 int main()
 {
 	setlocale(LC_ALL, "Ru");
 
-	int n = 3, m = 3;
-	int matrix[n][m];
-	long long  matrix_chek = get_matrix();
-	long long  matrix_pr = print_matrix();
+	srand(time(NULL));
+
+	int matrix[3][3];
+	for (int i = 1; i < 3 + 1; i++)
+	{
+		for (int j = 1; j < 3 + 1; j++)
+		{
+			matrix[i][j] = rand() % 10;
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+
 	int det;
 	int b, c, d;
+
 	b = matrix[1][1] * ((matrix[2][2] * matrix[3][3]) - (matrix[2][3] * matrix[3][2]));
 	c = matrix[1][2] * ((matrix[2][1] * matrix[3][3]) - (matrix[2][3] * matrix[3][1]));
 	d = matrix[1][3] * ((matrix[2][1] * matrix[3][2]) - (matrix[2][2] * matrix[3][1]));
 	det = b - c + d;
-	cout <<"Определитель матрицы ="<< det <<endl;
-	system("pause");
+	cout << "Определитель матрицы =" << det << endl;
+	return 0;
 }
